3a9efaedc5b4addceeb035d4c12fe640ba5aa88f
jamooooong <102500033+jamooooong@users.noreply.github.com>
2025-09-24 11:04:52 +0900

    Refactor "/stat" route component to include a TODO for future enhancements and improve formatting.

--- src/routes/stat.tsx
+++ src/routes/stat.tsx
@@ -1,9 +1,16 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/stat')({
+export const Route = createFileRoute("/stat")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/stat"!</div>
+  return (
+    <div>
+      Hello "/stat"!
+      {/* TODO */}
+      {/* /recommendations api에서 받은 데이터 사용자가 보기 편하게 통계
+    원하는 정보를 얻을 수 있어야함 라이브러리 설치 자유 */}
+    </div>
+  );
 }
